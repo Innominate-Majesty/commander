89481a6ec987ee5581c247c5426bc07a3ea529df
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2023-11-09 15:36:34 -0800

    Outline needed operations/commands

--- src/main/java/sjcc/App.java
+++ src/main/java/sjcc/App.java
@@ -5,10 +5,46 @@ package sjcc;
  * For CIS 54 - Data Structure
  *
  */
+
+import java.util.Scanner;
+import java.util.Stack;
+
 public class App 
 {
+
+    private Stack<String> commandStack;
+    private Stack<String> undoStack;
+    
+    public App() {
+
+    }
+
+    public void start() {
+
+    }
+
+    private void issueCommand() {
+
+    }
+
+    private void listCommands() {
+
+    }
+
+    private void undoCommand() {
+
+    }
+
+    private void redoCommand() {
+
+    }
+
+    private void quit() {
+
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
